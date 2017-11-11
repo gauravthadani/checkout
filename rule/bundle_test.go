a3fe5f36e02da9c921826c45ebfc50d65504a7e0
Gaurav Thadani <gaurav.shanker@yahoo.com>
2017-11-12 03:26:02 +0530

    add failing test for bundle product * more items in cart than bundled items

--- rule/bundle_test.go
+++ rule/bundle_test.go
@@ -20,6 +20,24 @@ func TestBundleRule(t *testing.T) {
 		t.Errorf("expected %f, got %f as discount for the bundle product", -5.0, discount)
 	}
 }
+
+//func TestBundleRule_MoreProductsThanBundled(t *testing.T) {
+//	productDef := NewSampleProduct()
+//	bundleDef := NewBundleProduct()
+//	rule := NewBundleRule(productDef, bundleDef)
+//
+//	cart := []product.Product{
+//		NewSampleProduct(),
+//		NewSampleProduct(),
+//		NewBundleProduct(),
+//	}
+//
+//	discount := rule.Evaluate(cart)
+//	if discount != -5.0 {
+//		t.Errorf("expected %f, got %f as discount for the bundle products", -5.0, discount)
+//	}
+//}
+
 func NewBundleProduct() product.Product {
 	return product.Product{
 		SKU:"cde",
