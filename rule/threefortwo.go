023d4151fb161ea36858c666e07f12b96b452677
Gaurav Thadani <gaurav.shanker@yahoo.com>
2017-11-12 01:45:17 +0530

    apply three for two rule for particular sku

--- rule/threefortwo.go
+++ rule/threefortwo.go
@@ -3,16 +3,28 @@ package rule
 import "github.com/gauravthadani/checkout/product"
 
 type ThreeForTwoPricingRule struct {
-
+	sku string
 }
 
-func NewThreeForTwoPricingRule() *ThreeForTwoPricingRule {
+func NewThreeForTwoPricingRule(sku string) *ThreeForTwoPricingRule {
 	return &ThreeForTwoPricingRule{
+		sku:sku,
 	}
 }
 
-func (rule *ThreeForTwoPricingRule) Evaluate(items []product.Product) (float64, error) {
-	return -100, nil
+func (rule *ThreeForTwoPricingRule) Evaluate(cart []product.Product) (float64, error) {
+	count := 0
+
+	for _, item := range cart {
+		if (item.SKU == rule.sku) {
+			count++
+		}
+	}
+
+	if (count >= 3) {
+		return -100, nil
+	}
+	return 0, nil
 }
 
 
--- rule/threefortwo_test.go
+++ rule/threefortwo_test.go
@@ -6,8 +6,19 @@ import (
 	"github.com/gauravthadani/checkout/product"
 )
 
-func Test3for2PricingRule(t *testing.T) {
-	rule := NewThreeForTwoPricingRule()
+func Test3for2PricingRule_InsufficientCart(t *testing.T) {
+	rule := NewThreeForTwoPricingRule("abc")
+
+	cartItems := []product.Product{NewSampleProduct()}
+
+	discount, _ := rule.Evaluate(cartItems)
+	if (discount != 0.00) {
+		t.Errorf("expected %f, got %f as no discount for single item in cart", 0.0, discount)
+	}
+}
+
+func Test3for2PricingRule_SufficientCart(t *testing.T) {
+	rule := NewThreeForTwoPricingRule("abc")
 
 	cartItems := []product.Product{NewSampleProduct(), NewSampleProduct(), NewSampleProduct() }
 
@@ -15,7 +26,6 @@ func Test3for2PricingRule(t *testing.T) {
 	if (discount != -100.0) {
 		t.Errorf("expected %f, got %f to discount 3 items for the price of 2", -100, discount)
 	}
-
 }
 
 func NewSampleProduct() product.Product {
