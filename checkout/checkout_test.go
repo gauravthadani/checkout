45c64fbdfbc6bd441aab0b2ad0e9ef05a99adbee
Gaurav Thadani <gaurav.shanker@yahoo.com>
2017-11-12 00:53:15 +0530

    add sample scenario 1 test

--- checkout/checkout_test.go
+++ checkout/checkout_test.go
@@ -6,26 +6,47 @@ import (
 	"github.com/gauravthadani/checkout/product"
 )
 
-func TestCheckoutSimpleTotal(t *testing.T) {
-
-	atv1 := product.Product{
-		Name:"Apple TV",
-		SKU:"atv",
-		Price:109.50,
-	}
-	atv2 := product.Product{
-		Name:"Apple TV",
-		SKU:"atv",
-		Price:109.50,
-	}
-
+func TestCheckoutTotalAppleTVs(t *testing.T) {
 	checkout := NewStoreCheckout()
-	checkout.Scan(atv1)
-	checkout.Scan(atv2)
+
+	checkout.Scan(NewAppleTV())
+	checkout.Scan(NewAppleTV())
 
 	total, _ := checkout.Total()
 	if (total != 219.0) {
-		t.Errorf("Expected %f, got %f", 219.0, total)
+		t.Errorf("Expected $%f, got $%f for 2 Apple TV's", 219.0, total)
 	}
-
 }
+
+func TestCheckoutTotalSampleScenario(t *testing.T){
+	checkout := NewStoreCheckout()
+
+	checkout.Scan(NewAppleTV())
+	checkout.Scan(NewAppleTV())
+	checkout.Scan(NewAppleTV())
+
+	checkout.Scan(NewVGA())
+
+	total, _:=checkout.Total()
+	if(total != 249.0) {
+		t.Errorf("Expected $%f , got $%f for 3 Apple TV's and a VGA adapter", 249.0, total)
+	}
+}
+
+func NewVGA() product.Product{
+	return product.Product{
+		Name:"VGA adapter",
+		SKU:"vga",
+		Price:30.0,
+	}
+}
+
+func NewAppleTV() product.Product {
+	return product.Product{
+		Name:"Apple TV",
+		SKU:"atv",
+		Price:109.50,
+	}
+}
+
+
